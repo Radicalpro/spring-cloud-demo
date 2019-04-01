eb3e330378b2c4f0183aacce09a8f57903a0b083
tianyi <793187547@qq.com>
2019-04-01 16:08:11 +0800

    注册中心

--- src/main/java/com/ty/demo/CloudApplication.java
+++ src/main/java/com/ty/demo/CloudApplication.java
@@ -2,6 +2,7 @@ package com.ty.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
+@EnableConfigServer
 public class CloudApplication {
 
     public static void main(String[] args) {

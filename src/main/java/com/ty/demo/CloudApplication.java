400f6c88be69c91bc6301a5697e12774592b4c69
tianyi <793187547@qq.com>
2019-04-03 15:33:06 +0800

    add config center

--- src/main/java/com/ty/demo/CloudApplication.java
+++ src/main/java/com/ty/demo/CloudApplication.java
@@ -2,7 +2,6 @@ package com.ty.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -13,7 +12,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
-@EnableConfigServer
 public class CloudApplication {
 
     public static void main(String[] args) {
